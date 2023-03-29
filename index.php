4baa3b6efd0103b158a87aaacb6cd0b51069921d
ashabilsyauqi <ashabilsyauqi@gmail.com>
2023-03-30 05:16:27 +0700

    Update by Ashabil isi file

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link icon -->
+    <!-- link css -->
+    <!-- link external font -->
+    <title>Document</title>
+</head>
+<body>
+<div class="container"> <!-- selalu buat content didalam container -->
+    <div id="root"></div> <!-- pemanggilan DOM di Javascript -->
+</div>
+<!-- link js -->
+</body>
+</html>
